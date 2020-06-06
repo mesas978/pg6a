171c41353c6915055467678fc713081f1de25675
Michael E Sweeney <info@cogswatch.org>
2020-06-06 16:30:27 -0600

    PostgreSQL is DATABASE_PASSWORD environment variable

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,6 +9,19 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("complaint") { req in
+        Complaint.query(on: req.db).all()
+    }
+
+    app.post("complaint") { req -> EventLoopFuture<Complaint> in
+        let complaint = try req.content.decode(Complaint.self)
+        if let id = complaint.id {
+            print("60p \(id)") // Optional(6005)
+        }
+        return complaint.save(on: req.db)
+            .map { complaint }
+    }
 
     try app.register(collection: TodoController())
 }
